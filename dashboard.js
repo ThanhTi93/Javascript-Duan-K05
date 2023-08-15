d6f96d5c83f7fa7ea84de78af86a918e60f68d8a
Phanvanduan2806 <Phanvanduan2806@gmail.com>
2023-08-15 11:58:01 +0700

    Duan

--- dashboard.js
+++ dashboard.js
@@ -81,25 +81,25 @@ contentMenuTo.forEach((elements, index) => {
         boxElement[index].style.display = "block";
         // menu mobile
         contentMenuMin.forEach((elements, i) => {
-            if(i === index){
+            if (i === index) {
                 elements.classList.add('nav--link--active')
-            }else{
+            } else {
                 elements.classList.remove('nav--link--active')
             }
         })
-          // menu nho
-          contentMenuNho.forEach((elements, i) => {
-            if(i === index){
+        // menu nho
+        contentMenuNho.forEach((elements, i) => {
+            if (i === index) {
                 elements.classList.add('nav--link--active')
-            }else{
+            } else {
                 elements.classList.remove('nav--link--active')
             }
         })
-          // menu lon
-          contentMenuTo.forEach((elements, i) => {
-            if(i === index){
+        // menu lon
+        contentMenuTo.forEach((elements, i) => {
+            if (i === index) {
                 elements.classList.add('nav--link--active')
-            }else{
+            } else {
                 elements.classList.remove('nav--link--active')
             }
         })
@@ -188,15 +188,15 @@ function tablePosts() {
             const tablesList = document.getElementById('booking');
 
             const tableSelect = document.getElementById('table--select');
-            const filterOption = data.filter((table,index) => {
+            const filterOption = data.filter((table, index) => {
                 return table.status == false;
             })
             // 2.Hiển thị dữ liệu lấy từ JSON Server
             filterOption.forEach(table => {
                 const listItemTable = document.createElement('option');
-                 listItemTable.value = table.id;
-                 listItemTable.textContent = ` Bàn ${table.id}`;
-                   tableSelect.appendChild(listItemTable);
+                listItemTable.value = table.id;
+                listItemTable.textContent = ` Bàn ${table.id}`;
+                tableSelect.appendChild(listItemTable);
             })
             // Hiển thị dữ liệu lấy từ JSON Server
             data.forEach(table => {
@@ -219,16 +219,17 @@ function tablePosts() {
                                      <img src="${imageBooking}" alt="">
                                  </div>
                                  <div class="bg__btn__booking" style="${listBtnAddCard}">
-                                    <button type="button" class="btn btn-info">
+                                    <button type="button" class="btn btn-info" onclick="clickAdd(${table.id})">
                                         <span class="btn-label"><i class="fa-solid fa-circle-plus"></i></span>Add
                                     </button>
-                                    <button type="button" class="btn btn-danger">
+                                    <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                                    data-bs-target="#listFoodOder" onclick="clickCard(${table.id})">
                                         <span class="btn-label"><i class="fa-solid fa-cart-plus"></i></span>Card
                                     </button>
                                 </div>
                                 <div class="bg__btn__booking" style="${listBtnBooking}">
                                     <button type="button" class="btn btn-warning" data-bs-toggle="modal"
-                                    data-bs-target="#staticBackdrop" onclick="clickBooking(${table.id})">
+                                    data-bs-target="#bookingTable" onclick="clickBooking(${table.id})">
                                         <span class="btn-label"><i class="fa-solid fa-circle-plus"></i></span>Booking
                                     </button>
                                 </div>
@@ -242,17 +243,77 @@ function tablePosts() {
 // Gọi hàm fetchPosts để lấy dữ liệu khi trang web được tải
 tablePosts();
 var id = null;
-function clickBooking(tableID){
+function clickBooking(tableID) {
     console.log(tableID);
-     id = tableID;
+    id = tableID;
 };
-// Update
+
+// Tạo biến đếm toàn cục
+var selectedTableId = null;
+// Click nút card để xem thông tin món ăn đã chọn
+
+function clickCard(tableId) {
+    const tdListFoodOder = document.querySelector('.td--list--food--oder');
+    console.log(tableId);
+    tdListFoodOder.innerHTML = '';
+    fetch('http://localhost:3000/orders')
+        .then(response => response.json())
+        .then(data => {
+            const orderForTable = data.filter(oder => oder.idTable == tableId);
+            const stt = 0;
+            console.log("Đây là odder của table", orderForTable);
+            orderForTable.forEach(order => {
+                order.items.forEach((item) => {
+                    const totalPrice = parseInt(item.quantity) * parseInt(item.priceItemFood);
+                    console.log("Lặp chưa", totalPrice);
+
+                    const row = `
+                <tr>
+                <th scope="row">${stt}</th>
+                <td class="td__food__image"><img src="${item.imageItemFood}" alt=""></td>
+                <td>${item.foodName}</td>
+                <td>${item.quantity}</td>
+                <td>${totalPrice} $</td>
+              </tr>
+                `;
+                    console.log('Row ở đâu:', row);
+                    tdListFoodOder.innerHTML += row;
+
+                });
+            });
+        })
+        .catch(error => console.error('Error fetching data:', error));
+}
+// Click nút Add
+// async function initialize(){
+//     await tablePosts();
+//     await foodPosts();
+// }
+
+function clickAdd(tableID) {
+    console.log(tableID);
+    // css lại nav và ẩn thèn đặt bàn 
+    boxElement[1].style.display = "none";
+    contentMenuTo[1].classList.remove('nav--link--active');
+    contentMenuNho[1].classList.remove('nav--link--active');
+    contentMenuMin[1].classList.remove('nav--link--active');
+    // // css lại nav và hiện thèn oder food
+    boxElement[2].style.display = "block";
+    contentMenuTo[2].classList.add('nav--link--active');
+    contentMenuNho[2].classList.add('nav--link--active');
+    contentMenuMin[2].classList.add('nav--link--active');
+    const id = document.getElementById('table--select');
+    id.value = tableID;
+}
+// initialize();
+
+// Update 
 document.getElementById('btn--booking').addEventListener('click', function () {
 
     const custumerName = document.getElementById('custumerName').value;
     const quantity = document.getElementById('quantity').value;
 
- 
+
     const newTable = {
         tableID: id,
         custumerName: custumerName,
@@ -269,11 +330,11 @@ document.getElementById('btn--booking').addEventListener('click', function () {
     })
         .then((response) => response.json())
         .then((data) => {
-             console.log(data);
-           
+            console.log(data);
+
         })
         .catch(error => console.error('Error creating post:', error));
-       
+
 });
 
 
@@ -295,13 +356,14 @@ function foodPosts() {
                 const listItem = document.createElement('div');
                 const foodName = food.foodName;
                 const foodImage = food.foodImage;
-// add class
+                const foodPrice = food.foodPrice;
+                // add class
                 listItem.classList.add("col-12");
                 listItem.classList.add("col-sm-6");
                 listItem.classList.add("col-md-4");
                 listItem.classList.add("col-lg-3");
                 listItem.classList.add("dish");
-// in ra html
+                // in ra html
                 listItem.innerHTML = `       
                 <div class="item__Foods">
                     <div class="edit__food">
@@ -310,14 +372,15 @@ function foodPosts() {
                         </button>
                     </div>
                     <div class="food__id">
-                        <span data-itemFood-id="">${food.id}</span>
+                        <span id-food="${food.id}">${food.id}</span>
                     </div>
                 <div class="food__name">
                     <span>${foodName}</span>
                 </div>
                 <div class="food__image">
-                    <img src="${foodImage}" alt="">
+                    <img src="${foodImage}" alt="" class="imgdish">
                 </div>
+                <div class="food__price">${foodPrice}</div>
                 <div class="food__number">
                     <button class="reduce__quantity__food"><span>-</span></button>
                     <input type="text" name="" class="quantity__food" value="0" readonly>
@@ -325,35 +388,35 @@ function foodPosts() {
                 </div>
             </div>
           `;
-          foodList.appendChild(listItem);
-        //   tạo biến tăng giảm số lượng sản phẩm
-          const reducaQuantityFood = listItem.querySelector('.reduce__quantity__food');
-          const addQuantityFood = listItem.querySelector('.add__quantity__food');
-          const quantityFood = listItem.querySelector('.quantity__food');
-        //   action của biến giảm
-          reducaQuantityFood.addEventListener('click', () => {
-            console.log("vào đây");
-            let valueQuantityFood = parseInt(quantityFood.value);
-            if(valueQuantityFood > 0){
-                quantityFood.value = (valueQuantityFood - 1).toString();
-            }
-          });
-        //   action của biến tăng
-          addQuantityFood.addEventListener('click', () => {
-            console.log("vào nut +");
-            let valueQuantityFood = parseInt(quantityFood.value);
-            quantityFood.value = (valueQuantityFood + 1).toString();
-          });
-       
+                foodList.appendChild(listItem);
+                //   tạo biến tăng giảm số lượng sản phẩm
+                const reducaQuantityFood = listItem.querySelector('.reduce__quantity__food');
+                const addQuantityFood = listItem.querySelector('.add__quantity__food');
+                const quantityFood = listItem.querySelector('.quantity__food');
+                //   action của biến giảm
+                reducaQuantityFood.addEventListener('click', () => {
+                    console.log("vào đây");
+                    let valueQuantityFood = parseInt(quantityFood.value);
+                    if (valueQuantityFood > 0) {
+                        quantityFood.value = (valueQuantityFood - 1).toString();
+                    }
+                });
+                //   action của biến tăng
+                addQuantityFood.addEventListener('click', () => {
+                    console.log("vào nut +");
+                    let valueQuantityFood = parseInt(quantityFood.value);
+                    quantityFood.value = (valueQuantityFood + 1).toString();
+                });
+
 
             });
-             //  tạo 1 function để sumbit các món ăn lên data
-        //   function addOrder(){
-        //     const tableId = document.getElementById('tableId');
-        //     const itemFoods = document.querySelectorAll('.item__Foods');
-        //     console.log(tableId);
-        // }
-        // addOrder();
+            //  tạo 1 function để sumbit các món ăn lên data
+            //   function addOrder(){
+            //     const tableId = document.getElementById('tableId');
+            //     const itemFoods = document.querySelectorAll('.item__Foods');
+            //     console.log(tableId);
+            // }
+            // addOrder();
         })
         .catch(error => console.error('Error fetching data:', error));
 }
@@ -363,27 +426,47 @@ function foodPosts() {
 
 
 
-function clickOption(){
+function clickOption() {
 
 }
 // Tạo biến đếm đơn hàng
 let currenOderId = 1;
-function addOrder(){
-    // const tableIdss = document.getElementById('table--select').value;
-    const itemFoods = document.querySelectorAll('.dish');
- 
-    // const idRow = document.getElementById('oderFood');
-    console.log(itemFoods);
-    // console.log(itemFoods);
-    // const orderFoodItems = [];
-    // itemFoods.forEach((itemFoods) => {
-    //     const quantity = parseInt(itemFoods.querySelector('.quantity__food').value);
-    //     if(quantity > 0){
-    //         const itemFoodId = itemFoods.getAttribute('data-itemFood-id');
-    //         console.log(itemFoodId);
-    //         console.log('Vào đây')
-    //     }
-    // })
+function addOrder(event) {
+    event.preventDefault();
+    const idTable = document.getElementById('table--select').value;
+    const itemFoods = document.querySelectorAll('.item__Foods');
+
+    const idFood = document.getElementById('oderFood');
+    const orderFoodItems = [];
+    itemFoods.forEach((itemFoods) => {
+        const quantity = parseInt(itemFoods.querySelector('.quantity__food').value);
+        if (quantity > 0) {
+            const foodName = itemFoods.querySelector('.food__name').innerText;
+            const idItemFood = itemFoods.querySelector('.food__id').innerText;
+            const imageItemFood = itemFoods.querySelector('.imgdish').getAttribute('src');
+            const priceItemFood = itemFoods.querySelector('.food__price').innerText;
+            orderFoodItems.push({ foodName, idItemFood, imageItemFood, quantity, priceItemFood });
+            itemFoods.querySelectorAll('.quantity__food').value = "0";
+        }
+    });
+    if (orderFoodItems.length > 0) {
+        const order = {
+            idTable,
+            items: orderFoodItems
+        }
+        fetch('http://localhost:3000/orders', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(order),
+        })
+        then((response) => response.json())
+            .then((data) => {
+                console.log('Đơn hàng đã được thêm', data);
+                currenOderId++;
+            })
+            .catch(error => console.error('Lỗi khi thêm đơn hàng', error));
+    }
 }
-addOrder();
 foodPosts();
